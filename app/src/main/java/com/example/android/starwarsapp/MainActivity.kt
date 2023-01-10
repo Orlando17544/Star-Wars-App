f11bffa7e4022b8f82f0fcfdf7c3484686887cee
Orlando Canchola Jimenez <orcj17544@gmail.com>
2023-01-10 12:53:15 -0600

    View model and data binding added to the main activity

--- app/src/main/java/com/example/android/starwarsapp/MainActivity.kt
+++ app/src/main/java/com/example/android/starwarsapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.android.starwarsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.starwarsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.viewModel = viewModel;
+
+        binding.lifecycleOwner = this;
     }
 }
